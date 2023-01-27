d20000c5010a2938692dcb681336bbad83664d69
Andrés Marcote Fernandez <amarfer@aulaestudio.es>
2023-01-27 16:18:50 +0100

    Update

--- PracticaMusica/script.js
+++ PracticaMusica/script.js
@@ -4,6 +4,8 @@ const song1 = document.getElementById("song1");
 const song2 = document.getElementById("song2");
 const song3 = document.getElementById("song3");
 
+let checkAnimation = false; //Variable para controlar si la ani
+
 song1.addEventListener('play', () => {
     song2.pause();
     song2.currentTime = 0;
@@ -28,16 +30,25 @@ song3.addEventListener('play', () => {
 
 function revealMusic() {
     let reveals = document.querySelectorAll(".reveal");
-
     for (let i = 0; i < reveals.length; i++) {
         let windowHeight = window.innerHeight;
         let elementTop = reveals[i].getBoundingClientRect().top;
         let elementVisible = 130;
-
-        if (elementTop < windowHeight - elementVisible) {
-            reveals[i].classList.add("active");
+        if (checkAnimation) {
+            console.log("entro check");
+            reveals.forEach((element) => {
+                element.classList.remove("reveal");
+            })
         } else {
-            reveals[i].classList.remove("active");
+            if (elementTop < windowHeight - elementVisible) {
+                reveals[i].classList.add("active");
+                if (i == reveals.length - 1) {
+                    console.log("Entro en length");
+                    checkAnimation = true;
+                }
+            } else {
+                reveals[i].classList.remove("active");
+            }
         }
     }
 }
