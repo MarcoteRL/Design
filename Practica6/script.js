c4f9752391a944ad7f97b23f471d84348eff98e0
Marcote <andresmarcote13@gmail.com>
2023-02-10 17:44:00 +0100

    Update

---
+++ Practica6/script.js
@@ -0,0 +1,69 @@
+"use strict";
+
+let citas = [
+]
+
+console.log(citas.includes("09.00"));
+
+async function setTimeAppointments() {
+    // $("#hora")
+    let pushed = [];
+    for (let i = 0; i <= 23; i++) {
+        if (citas.length > 0) {
+            citas.forEach((element) => {
+                if (i < 10 && !pushed.includes(`${i}:00`)) {
+                    i = `0${i}`;
+                }
+                if (element.fecha != `${i}:00` && !pushed.includes(`${i}:00`)) {
+                    $('#hora').append(`<option value="${i}:00">${i}:00</option>`);
+                    pushed.push(`${i}:00`)
+                }
+            })
+        } else {
+            if (i < 10 && !pushed.includes(`${i}:00`)) {
+                i = `0${i}`;
+            }
+            if (!pushed.includes(`${i}:00`)) {
+                $('#hora').append(`<option value="${i}:00">${i}:00</option>`);
+                pushed.push(`${i}:00`)
+            }
+        }
+    }
+}
+
+async function addCita() {
+    console.log($("#hora option:selected").text());
+    citas.push({
+        nombre: $("input[name=nombre]").val(),
+        fecha: $("input[name=fecha]").val(),
+        hora: $("#hora option:selected").text()
+    }
+    );
+    await setTimeAppointments();
+    await showTable();
+}
+
+$("#formulario").submit((e) => {
+    e.preventDefault();
+    addCita();
+})
+
+async function showTable() {
+    $("table").remove();
+    let table = `<table class="table table-responsive">`;
+    table += `<tr>
+    <th scope="col">Nombre</th>
+    <th scope="col">Fecha</th>
+    <th scope="col">Hora</th>
+    </tr>`;
+
+    for (let i = 0; i < citas.length; i++) {
+        console.log(citas[i].nombre);
+        table += '<tr><td>' + citas[i].nombre + '</td><td>' + citas[i].fecha + '</td><td>' + citas[i].hora + '</td></tr>';
+    }
+    table += "</table>";
+    $('#table-responsive').append(table);
+}
+
+setTimeAppointments();
+showTable();
