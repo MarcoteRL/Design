4a5e8a62e8340f944e173f36940071cc39f20061
Marcote <andresmarcote13@gmail.com>
2023-02-15 17:22:55 +0100

    Update

--- Practica6/script.js
+++ Practica6/script.js
@@ -3,18 +3,39 @@
 let citas = [
 ]
 
-console.log(citas.includes("09.00"));
+let dt = new Date();
+let year = dt.getFullYear();
+let month = "";
+let day = "";
+let mes = dt.getMonth() + 1
+if (dt.getMonth() < 10) {
+    month = "0" + mes;
+} else {
+    month = mes;
+}
+if (dt.getDate() < 10) {
+    day = "0" + dt.getDate();
+} else {
+    day = dt.getDate();
+}
+let fechaHoy = {
+    year: year,
+    month: month,
+    day: day
+}
 
-async function setTimeAppointments() {
-    // $("#hora")
+let hoy = year + "-" + month + "-" + day;
+
+
+async function setTimeAppointment() {
     let pushed = [];
-    for (let i = 0; i <= 23; i++) {
+    for (let i = 9; i <= 21; i++) {
         if (citas.length > 0) {
             citas.forEach((element) => {
                 if (i < 10 && !pushed.includes(`${i}:00`)) {
                     i = `0${i}`;
                 }
-                if (element.fecha != `${i}:00` && !pushed.includes(`${i}:00`)) {
+                if (element.hora != `${i}:00` && !pushed.includes(`${i}:00`)) {
                     $('#hora').append(`<option value="${i}:00">${i}:00</option>`);
                     pushed.push(`${i}:00`)
                 }
@@ -33,19 +54,37 @@ async function setTimeAppointments() {
 
 async function addCita() {
     console.log($("#hora option:selected").text());
-    citas.push({
-        nombre: $("input[name=nombre]").val(),
-        fecha: $("input[name=fecha]").val(),
-        hora: $("#hora option:selected").text()
+    if (citas.length == 0) {
+        citas.push({
+            nombre: $("input[name=nombre]").val(),
+            fecha: $("input[name=fecha]").val(),
+            hora: $("#hora option:selected").text()
+        }
+        );
+        return;
     }
-    );
-    await setTimeAppointments();
-    await showTable();
+    if (citas.some((element) => {
+        let splitted = $("input[name=fecha]").val().split("-");
+        return element.fecha == $("input[name=fecha]").val() &&
+            element.hora == $("#hora option:selected").text() || splitted[0] < fechaHoy.year ||
+            splitted[1] < fechaHoy.month || splitted[2] < fechaHoy.day
+
+    })) {
+        return;
+    } else {
+        citas.push({
+            nombre: $("input[name=nombre]").val(),
+            fecha: $("input[name=fecha]").val(),
+            hora: $("#hora option:selected").text()
+        }
+        );
+    }
 }
 
-$("#formulario").submit((e) => {
+$("#formulario").submit(async (e) => {
     e.preventDefault();
-    addCita();
+    await addCita();
+    await showTable();
 })
 
 async function showTable() {
@@ -65,5 +104,5 @@ async function showTable() {
     $('#table-responsive').append(table);
 }
 
-setTimeAppointments();
+setTimeAppointment();
 showTable();
